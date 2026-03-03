e55725efedee54a92e4113b0b4ba9c89376b9a1c
lakshmanan1411 <12953bca2023@princescience.in>
2026-03-03 20:50:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,4 +76,20 @@ function filterTasks(filter) {
     currentFilter = filter;
     renderTasks();
 }
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = function() {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+    }
+};
+
 
